c0ca46ddbd18a93c4f568e3b96125f60ddc1393a
kravchenkoevgen <kravchenkoevgen@nvk6.ukr.education>
2021-07-25 22:32:19 +0300

    replace for-цикл .gitignore

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 // alert("hw")
 console.log("hw")
+
+
+let str1 = "str"
+var str2 = "str"
+const str3 = "str"
+
+str1.replace("s", "S")
+
+
+console.log(str1.replace("s", "S"))
+
+for (let i=0; i<5; i++) {
+    console.log(i)
+}
